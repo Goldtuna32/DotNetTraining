d166acfb8e8dbe515f73ed6a42d1e7173ccf50d8
minza <minza@DESKTOP-UUEE2ME>
2025-11-03 23:06:06 +0630

    Data Reading from sql server after closing connection

--- DotNetTraining.ConsoleApp/Program.cs
+++ DotNetTraining.ConsoleApp/Program.cs
@@ -1,3 +1,34 @@
 ﻿// See https://aka.ms/new-console-template for more information
+using System.Data;
+using System.Data.SqlClient;
+
 Console.WriteLine("Hello, World!");
-Console.ReadLine();
+
+string connectionString = "Data Source=DESKTOP-UUEE2ME\\SQLEXPRESS;Initial Catalog=DotNetTraining5;User ID=sa;Password=17112006";
+SqlConnection connection = new SqlConnection(connectionString);
+connection.Open();
+Console.WriteLine("Connection is opened");
+
+string query = @"SELECT [Blog_id]
+      ,[Blog_title]
+      ,[Blog_author]
+      ,[Blog_context]
+  FROM [dbo].[blogTable]";
+SqlCommand cmd = new SqlCommand(query, connection);
+
+// for each looping (Data reading after closing connection) //
+SqlDataAdapter adapter = new SqlDataAdapter(cmd);
+DataTable dt = new DataTable();
+adapter.Fill(dt);
+
+connection.Close();
+Console.WriteLine("Connection is closed");
+
+foreach (DataRow dr in dt.Rows)
+{
+    Console.WriteLine(dr["Blog_title"]);
+    Console.WriteLine(dr["Blog_author"]);
+    Console.WriteLine(dr["Blog_context"]);
+}
+
+Console.ReadKey();
